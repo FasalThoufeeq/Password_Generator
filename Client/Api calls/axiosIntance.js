af9ac5b50d976c62ed8708a0306844a5f221777b
Fasal Thoufeeq <fasaltq681@gmail.com>
2023-09-15 11:44:35 +0530

    after hosted backend

--- Client/Api calls/axiosIntance.js
+++ Client/Api calls/axiosIntance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosUserInstance = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://password-generator-85ob.onrender.com',
 })
